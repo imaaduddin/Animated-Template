86496e64a6f00a674f861fac6a7f5b86bd0d4ee6
Imaad Uddin <imaaduddin@icloud.com>
2021-03-03 11:37:08 -0600

    js files

---
+++ navbar.js
@@ -0,0 +1,5 @@
+// Navbar
+function toggleNavbar(collapseID) {
+  document.getElementById(collapseID).classList.toggle("hidden");
+  document.getElementById(collapseID).classList.toggle("block");
+}
---
+++ script.js
@@ -0,0 +1,7 @@
+// AOS JS
+AOS.init({
+  delay: 200,
+  duration: 1500,
+  once: false,
+  mirror: false,
+});
